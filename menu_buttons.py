eb229eab843233a7b54545537e994d366b33e24c
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2024-12-17 16:05:47 +0300

    all buttons were replaced to the separate file

--- menu_buttons.py
+++ menu_buttons.py
@@ -4,6 +4,7 @@ from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
 
 from constants import *
+from keyboards import *
 
 from test_db import get_user_from_db, update_user_in_db
 
@@ -16,24 +17,11 @@ async def main_start_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
                 await update_user_in_db(user_id, menu_state='Меню администратора')
                 context.user_data['menu_state'] = 'Меню администратора'
                 text = 'Выберите опцию'
-        keyboard = [
-                ['Вакансии'],
-                ['О компании'],
-                ['Частые вопросы'],
-                ['Лист ожидания'],
-                ['Контакты'],
-                ['Панель администратора']
-            ]
+        keyboard = admin_main_menu_keyboard
     else:
         context.user_data['menu_state'] = 'Меню пользователя'
         await update_user_in_db(user_id, menu_state='Меню пользователя')
-        keyboard = [
-                ['Вакансии'],
-                ['О компании'],
-                ['Частые вопросы'],
-                ['Лист ожидания'],
-                ['Контакты'],
-            ]
+        keyboard = user_main_menu_keyboard
     # Создаем разметку клавиатуры
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     
@@ -45,10 +33,7 @@ async def show_admin_options(update: Update, context: ContextTypes.DEFAULT_TYPE)
     user_id = update.effective_user.id
     await update_user_in_db(user_id, menu_state='Панель администратора')
     
-    keyboard = [
-        ['Рассылка'],
-        ['Главное меню']
-    ]
+    keyboard = admin_options_keyboard
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text('Выберите опцию', reply_markup=reply_markup)
 
@@ -58,12 +43,7 @@ async def show_vacancies(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     await update_user_in_db(user_id, menu_state='Меню вакансий')
     context.user_data['menu_state'] = 'Меню вакансий'
     
-    keyboard = [
-        ['Посмотреть все вакансии'],
-        ['Категории'],
-        ['Вакансии без опыта'],
-        ['Главное меню']
-    ]
+    keyboard = vacancies_menu_keyboard
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text('Выберите вариант вакансий', reply_markup=reply_markup)
 
@@ -73,11 +53,7 @@ async def show_all_vacancies_sure(update: Update, context: ContextTypes.DEFAULT_
     await update_user_in_db(user_id, menu_state='SURE?')
     
     
-    keyboard = [
-        ['Рискну'],
-        ['Главное меню'],
-
-    ]
+    keyboard = warning_options_keyboard
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     count = context.user_data['vacancies_count']
     
@@ -94,12 +70,7 @@ async def show_categories(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     await update_user_in_db(user_id, menu_state='Меню категорий')
 
     
-    keyboard = [
-        ['ТЭЦ'],
-        ['Офис'],
-        ['Стажировка'],
-        ['Главное меню']
-    ]
+    keyboard = vacancies_categories_keyboard
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text('Выберите нужную категорию', reply_markup=reply_markup)
 
@@ -109,15 +80,7 @@ async def show_power_vacancies(update: Update, context: ContextTypes.DEFAULT_TYP
     await update_user_in_db(user_id, menu_state='Меню вакансий ТЭЦ')
     
     
-    keyboard = [
-        ['Теплоэнергетика', 'Электроэнергетика'],
-        ['АСУ ТП', 'РЗА'],
-        ['Ремонт', 'Химия'],
-        ['HR', 'ИТ'],
-        ['Экономика', 'Сбыт'],
-        ['Промышленная безопасность и охрана труда'],
-        ['Другое', 'Главное меню']
-    ]
+    keyboard = power_vacancies_keyboard
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text('Выберите нужную категорию', reply_markup=reply_markup)
 
@@ -127,13 +90,7 @@ async def show_office_vacancies(update: Update, context: ContextTypes.DEFAULT_TY
     await update_user_in_db(user_id, menu_state='Меню вакансий офис')
     
     
-    keyboard = [
-        ['Закупки', 'Экономика'],
-        ['HR', 'Сбыт', 'ИТ'],
-        ['Юриспруденция', 'Производственное управление'],
-        ['Промышленная безопасность и охрана труда'],
-        ['Главное меню']
-    ]
+    keyboard = office_vacancies_keyboard
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text('Выберите нужную категорию', reply_markup=reply_markup)
 
@@ -143,12 +100,7 @@ async def show_about_company(update: Update, context: ContextTypes.DEFAULT_TYPE)
     await update_user_in_db(user_id, menu_state='О компании')
     
     
-    keyboard = [
-        ['Преимущества работы в ПАО «Мосэнерго»'],
-        ['Филиалы'],
-        ['Мотивационные и социальные программы'],
-        ['Главное меню'],
-    ]
+    keyboard = about_company_keyboard
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
     
@@ -161,15 +113,7 @@ async def show_motivations_programms(update: Update, context: ContextTypes.DEFAU
     await update_user_in_db(user_id, menu_state='Мотивационные программы')
     
     
-    keyboard = [
-        ['Кадровый резерв'],
-        ['Профессиональная подготовка и повышение квалификации за счет работодателя'],
-        ['Соревнования профессионального мастерства'],
-        ['Спортивные соревнования'],
-        ['Корпоративные культурно-массовые мероприятия'],
-        ['Совет молодых специалистов'],
-        ['Главное меню'],
-    ]
+    keyboard = motivations_programms_keyboard
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
     
@@ -182,18 +126,7 @@ async def show_FAQ(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update_user_in_db(user_id, menu_state='Частые вопросы')
     
     
-    keyboard = [
-        ['💼 По какому графику я буду работать?'],
-        ['💰 Какой уровень заработной платы мне ожидать?'],
-        ['🏠 Какие компенсации существуют для иногородних кандидатов?'],
-        ['🛡️ Какие социальные гарантии предоставляет Мосэнерго?'],
-        ['📍 Адреса ТЭЦ Мосэнерго ГЭС-1'],
-        ['👩‍🏫 На какие должности можно прийти без опыта?'],
-        ['📝 Какие документы мне понадобятся при трудоустройстве?'],
-        ['🚀 Есть ли в компании возможность карьерного роста?'],
-        ['Главное меню'],
-
-    ]
+    keyboard = FAQ_keyboard
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text('Выберите интересующий вопрос', reply_markup=reply_markup)
 
